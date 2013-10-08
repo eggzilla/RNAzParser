32655d386a16d15fdf456277e158e8729313f0da
Florian Eggenhofer <egg@tbi.univie.ac.at>
2013-10-08 13:58:09 +0200

    Added basic datastructure for RNAz Output

--- src/Bio/RNAzData.hs
+++ src/Bio/RNAzData.hs
@@ -1,10 +1,33 @@
 -- | This module contains a hierarchical data structure for smiles strings
 --   For more information on smiles strings consult: <http://meme.nbcr.net/meme/>
 
-module Chem.SmilesData where
+module Bio.RNAzData where
 
 -- | 
-data SmilesString = SmilesString
-  { identifier :: String,
-    smilesString ::String }
+data RNAzOutput = RNAzOutput
+  { version :: String,
+    sequenceNumber :: Int,
+    columnNumber :: Int,
+    readingDirection :: String,
+    meanPairwiseIdentity :: Double,
+    meanSingleSequenceMinimumFreeEnergy :: Double,
+    consensusMinimumFreeEnergy :: Double,
+    energyContribution :: Double,
+    covarianceContribution :: Double,
+    combinationsPair :: Double,
+    meanZScore :: Double,
+    structureConservationIndex :: Double,
+    svmDecisionValue :: Double,
+    svmRNAClassProbability :: Double,
+    prediction :: String,
+    rnazResults :: [RNAzResult]   
+  }
+  deriving (Show, Eq)
+
+
+data RNAzResult = RNAzResult
+  { header :: String,
+    sequence :: String,
+    mfe :: Double,
+  }
   deriving (Show, Eq)
